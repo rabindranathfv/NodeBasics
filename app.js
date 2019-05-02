2d63de38503bb2799a57f19481cf7060ea6a4001
Rabindranath Ferreira <ferreiralox@gmail.com>
2019-05-02 01:29:02 -0400

    write a txt file using filesystem package

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
+
 let base = 2;
+
+for (let index = 1; index < 10; index++) {
+    let element = base * index;
+    console.log(`${ index } * ${ base } = ${ element }`);
+}
+
+fs.writeFile('message.txt', 'hello node js', err => {
+    if (err) throw err;
+    console.log('the file has been saved');
+
+});
