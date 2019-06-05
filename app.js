2e4b2b182a7af0dd4d3fdfc3eb7fedc4557d880c
Rabindranath Ferreira <ferreiralox@gmail.com>
2019-06-05 02:07:59 -0400

    configure yars with basic options

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const argv = require('yargs')
+    .command('list', 'table of multiply using nodeJs', {
+        base: {
+            demand: true
+        }
+    })
+    .argv;
 // const multiply = require('./multiplicar/multiply');
 const { createFile } = require('./multiplicar/multiply');
 
-let argv = process.argv;
-let params = argv[2];
-let base = Number(params.split('=')[1]);
-console.log(base);
+// let argv = process.argv;
+// let params = argv[2];
+// let base = Number(params.split('=')[1]);
+// console.log(base);
 
 createFile(base).then(file => console.log(`archivo creado ${file}`))
     .catch(e => console.log(e));
