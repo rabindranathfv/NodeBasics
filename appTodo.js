d1fdba01ddc6a8ab363bb007f8878f748fb10307
rabindranathfv <rabindranathucv@gmail.com>
2019-06-06 12:57:49 -0400

    create todo task and safe into db as json file

--- appTodo.js
+++ appTodo.js
@@ -1,13 +1,17 @@
 const argv = require('./config/yargsTodo').argv;
 const colors = require('colors');
+const { createTask } = require('./todo/todo');
 
 console.log(argv);
 
 let command = argv._[0];
 
-switch(command) {
+switch (command) {
     case 'create':
         console.log('create a todo TASK'.blue);
+        let task = createTask(argv.description);
+        console.log(task);
+        console.log(`the task ${task.description} is succesfully created`.green);
         break;
     case 'list':
         console.log('List of Todos '.blue);
--- todo/todo.js
+++ todo/todo.js
@@ -1,2 +1,27 @@
 const fs = require('fs');
 const colors = require('colors');
+
+let todoList = [];
+
+const saveDb = () => {
+    let data = JSON.stringify(todoList);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('can save in db'.red);
+    });
+}
+
+const createTask = (description) => {
+    let todo = {
+        description,
+        completed: false
+    };
+    todoList.push(todo);
+    saveDb();
+    return todo;
+};
+
+
+module.exports = {
+    createTask
+}
