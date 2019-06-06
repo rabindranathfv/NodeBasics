92fd6b2635b43be9878875d71a0112fbf07e8ed6
rabindranathfv <rabindranathucv@gmail.com>
2019-06-06 19:01:54 -0400

    save todos into data.json

--- todo/todo.js
+++ todo/todo.js
@@ -1,27 +1,36 @@
 const fs = require('fs');
 const colors = require('colors');
+const db = require('../db/data.json');
 
 let todoList = [];
+
+const loadDb = () => {
+    todoList = [...db];
+    console.log('ALL DB', todoList);
+    console.log('*********');
+}
+
 
 const saveDb = () => {
     let data = JSON.stringify(todoList);
 
     fs.writeFile('db/data.json', data, (err) => {
-        if (err) throw new Error('can save in db'.red);
+        if (err) throw new Error('can save in db'.red, err);
     });
 }
 
 const createTask = (description) => {
-    let todo = {
+    loadDb();
+    let newTodo = {
         description,
         completed: false
     };
-    todoList.push(todo);
+    todoList.push(newTodo);
     saveDb();
-    return todo;
+    return newTodo;
 };
 
 
 module.exports = {
     createTask
-}
+};
